8d643e3b41b830fd4c88ddae54705293456ab50a
drdholu <paras.dhole@outlook.com>
2025-06-04 09:55:57 +0530

    feat: updated proto and build files

--- client/build.rs
+++ client/build.rs
@@ -1,7 +1,14 @@
-use std::fmt::Error;
+use std::{env, error::Error, path::PathBuf};
 
+fn main()-> Result<(),Box<dyn Error>>{
 
-fn main()-> Result<(),Box<dyn std::error::Error>>{
-    tonic_build::compile_protos("../proto/ftp.proto");
+    // reflection
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    
+    let _ = tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("ftp_descriptor.bin"))
+        .compile_protos(&["proto/ftp.proto"], &["proto"]);
+
+    let _ = tonic_build::compile_protos("../proto/ftp.proto");
     Ok(())
 }
