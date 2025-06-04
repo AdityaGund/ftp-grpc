85e0bbe1c010181c3b1b1e360cb25a82c9bb74b9
drdholu <paras.dhole@outlook.com>
2025-06-04 10:38:08 +0530

    feat: added workspaces

--- client/build.rs
+++ client/build.rs
@@ -1,14 +1,16 @@
-use std::{env, error::Error, path::PathBuf};
+use std::{env, path::PathBuf};
 
-fn main()-> Result<(),Box<dyn Error>>{
 
-    // reflection
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+fn main()-> Result<(),Box<dyn std::error::Error>>{
     
-    let _ = tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("ftp_descriptor.bin"))
-        .compile_protos(&["proto/ftp.proto"], &["proto"]);
+    // reflections
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
+    let _ = tonic_build::configure()
+    .file_descriptor_set_path(out_dir.join("ftp_descriptor.bin"))
+    .compile_protos(&["../proto/ftp.proto"], &["proto"])
+    .unwrap();
+    
     let _ = tonic_build::compile_protos("../proto/ftp.proto");
     Ok(())
 }
--- server/build.rs
+++ server/build.rs
@@ -1,4 +1,4 @@
-use std::{env, fmt::Error, path::PathBuf};
+use std::{env, path::PathBuf};
 
 
 fn main()-> Result<(),Box<dyn std::error::Error>>{
@@ -7,9 +7,9 @@ fn main()-> Result<(),Box<dyn std::error::Error>>{
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     let _ = tonic_build::configure()
-    .file_descriptor_set_path(out_dir.join("ftp_descriptor.bin"))
-    .compile_protos(&["../proto/ftp.proto"], &["proto"])
-    .unwrap();
+        .file_descriptor_set_path(out_dir.join("ftp_descriptor.bin"))
+        .compile_protos(&["../proto/ftp.proto"], &["proto"])
+        .unwrap();
     
     let _ = tonic_build::compile_protos("../proto/ftp.proto");
     Ok(())
