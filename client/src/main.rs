2e7070b6f3e2d86d28b3878b00e2d97a8cd63719
drdholu <paras.dhole@outlook.com>
2025-06-04 14:34:58 +0530

    feat: basic code

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use ftp::transfer_service_client::TransferServiceClient;
+
+pub mod ftp {
+    tonic::include_proto!("ftp");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>>{
+    let url = "http://[::1]:50051";
+    let mut client = TransferServiceClient::connect(url).await?;
+    
+
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, net::SocketAddr};
+use tonic::transport::Server;
+use ftp::transfer_service_server::{TransferService, TransferServiceServer};
+use ftp::{TransferResponse, Chunk, Status};
+
+pub mod ftp {
+    tonic::include_proto!("ftp");
+
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("ftp_descriptor");
+}
+
+#[derive(Debug, Default)]
+pub struct TransferServiceImpl {}
+
+#[tonic::async_trait]
+impl TransferService for TransferServiceImpl {
+    async fn transfer(
+        &self,
+        request: tonic::Request<tonic::Streaming<Chunk>>,
+    ) -> Result<tonic::Response<TransferResponse>, tonic::Status> {
+        let response = TransferResponse {
+            transfer_id: "test".to_string(),
+            status: Status::Success as i32,
+            message: "Success".to_string(),
+        };
+        Ok(tonic::Response::new(response))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // println!("Hello, world!");
+
+    let addr: SocketAddr = "[::1]:50051".parse()?;
+    let service = TransferServiceImpl::default();
+
+    Server::builder()
+        .add_service(TransferServiceServer::new(service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
