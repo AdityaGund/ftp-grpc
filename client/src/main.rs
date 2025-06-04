0791a44ce0fe42b7dcfc61843dec2102316f02ed
drdholu <paras.dhole@outlook.com>
2025-06-04 16:29:26 +0530

    feat: basic msg transfer implemented

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use ftp::{TransferRequest, Metadata, FileInfo};
 
 use ftp::transfer_service_client::TransferServiceClient;
 
@@ -6,11 +7,35 @@ pub mod ftp {
     tonic::include_proto!("ftp");
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>>{
-    let url = "http://[::1]:50051";
-    let mut client = TransferServiceClient::connect(url).await?;
-    
+async fn transfer_file(client: &mut TransferServiceClient<tonic::transport::Channel>, file_path: &str) -> Result<(), Box<dyn Error>> {
+    // For grpcurl, you want to be able to send path, sender_bank_id, receiver_bank_id
+    // We'll just use the file_path and hardcode the rest for now
+    let file_info = FileInfo {
+        path: file_path.to_string(),
+        name: file_path.to_string(),
+        // size: 0, // Not used in proto for grpcurl, so set to 0
+    };
 
+    let metadata = Metadata {
+        transfer_id: "transfer-1".to_string(),
+        sender_bank_id: "A".to_string(), // You can change this for grpcurl
+        receiver_bank_id: "B".to_string(), // You can change this for grpcurl
+        payload_type: Some(ftp::metadata::PayloadType::FileInfo(file_info)),
+    };
+
+    let request = TransferRequest {
+        metadata: Some(metadata),
+    };
+
+    let response = client.transfer(request).await?;
+    println!("Response: {:?}", response.into_inner());
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "http://127.0.0.1:5051";
+    let mut client = TransferServiceClient::connect(url).await?;
+    transfer_file(&mut client, "../../ref.txt").await?;
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,28 +1,58 @@
+use ftp::transfer_service_server::{TransferService, TransferServiceServer};
+use ftp::{Status, TransferRequest, TransferResponse};
 use std::{error::Error, net::SocketAddr};
 use tonic::transport::Server;
-use ftp::transfer_service_server::{TransferService, TransferServiceServer};
-use ftp::{TransferResponse, Chunk, Status};
 
 pub mod ftp {
     tonic::include_proto!("ftp");
 
-    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+    pub(crate) const _FILE_DESCRIPTOR_SET: &[u8] =
         tonic::include_file_descriptor_set!("ftp_descriptor");
 }
 
 #[derive(Debug, Default)]
-pub struct TransferServiceImpl {}
+pub struct ClientTransfer {}
 
 #[tonic::async_trait]
-impl TransferService for TransferServiceImpl {
+impl TransferService for ClientTransfer {
     async fn transfer(
         &self,
-        request: tonic::Request<tonic::Streaming<Chunk>>,
+        request: tonic::Request<TransferRequest>,
     ) -> Result<tonic::Response<TransferResponse>, tonic::Status> {
+        println!("[SERVER] Transfer function invoked!");
+        let req = request.into_inner();
+        let transfer_id = req
+            .metadata
+            .as_ref()
+            .map(|m| m.transfer_id.clone())
+            .unwrap_or_default();
+
+        let file_name = req
+            .metadata
+            .as_ref()
+            .and_then(|m| match &m.payload_type {
+                Some(ftp::metadata::PayloadType::FileInfo(info)) => Some(info.name.clone()),
+                _ => None,
+            })
+            .unwrap_or_else(|| "no_file_name_in_metadata".to_string());
+
+        let file_path = req
+            .metadata
+            .as_ref()
+            .and_then(|m| match &m.payload_type {
+                Some(ftp::metadata::PayloadType::FileInfo(info)) => Some(info.path.clone()),
+                _ => None,
+            })
+            .unwrap_or_else(|| "no_path_in_metadata".to_string());
+
+        println!(
+            "[SERVER] Received transfer_id: {transfer_id}, file_name: {file_name}, file_path: {file_path}"
+        );
+        
         let response = TransferResponse {
-            transfer_id: "test".to_string(),
+            transfer_id,
             status: Status::Success as i32,
-            message: "Success".to_string(),
+            message: format!("Received file info: name={}, path={}", file_name, file_path),
         };
         Ok(tonic::Response::new(response))
     }
@@ -30,10 +60,9 @@ impl TransferService for TransferServiceImpl {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // println!("Hello, world!");
-
-    let addr: SocketAddr = "[::1]:50051".parse()?;
-    let service = TransferServiceImpl::default();
+    println!("[SERVER] Starting gRPC server on 127.0.0.1:5051");
+    let addr: SocketAddr = "127.0.0.1:5051".parse()?;
+    let service = ClientTransfer::default();
 
     Server::builder()
         .add_service(TransferServiceServer::new(service))
