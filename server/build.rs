11b0d9dcb0f1f1b2ccfc26f5164cfbfad045d222
drdholu <paras.dhole@outlook.com>
2025-06-04 10:00:52 +0530

    feat: added reflections to build.rs

--- server/build.rs
+++ server/build.rs
@@ -1,7 +1,16 @@
-use std::fmt::Error;
+use std::{env, fmt::Error, path::PathBuf};
 
 
 fn main()-> Result<(),Box<dyn std::error::Error>>{
-    tonic_build::compile_protos("../proto/ftp.proto");
+    
+    // reflections
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let _ = tonic_build::configure()
+    .file_descriptor_set_path(out_dir.join("ftp_descriptor.bin"))
+    .compile_protos(&["../proto/ftp.proto"], &["proto"])
+    .unwrap();
+    
+    let _ = tonic_build::compile_protos("../proto/ftp.proto");
     Ok(())
 }
