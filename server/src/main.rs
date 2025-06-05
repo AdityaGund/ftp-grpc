b1753546df68220930812d36030376aa29edfc51
drdholu <paras.dhole@outlook.com>
2025-06-05 11:24:39 +0530

    chore: remove destination folder

--- server/src/main.rs
+++ server/src/main.rs
@@ -17,7 +17,7 @@ pub struct FileTransferService {
 impl FileTransferService {
     pub fn new() -> Self {
         let mut mappings = HashMap::new();
-        mappings.insert("BANK_C".to_string(), "http://127.0.0.1:50052".to_string());
+        mappings.insert("BANK_C".to_string(), "http://192.168.16.1:50052".to_string());
         mappings.insert("BANK_D".to_string(), "http://127.0.0.1:50053".to_string());
         
         Self { bank_mappings: mappings }
